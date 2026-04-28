657df10587da9726b8717a7eb691f65ae76b924c
Thasniya Thaha <Thasniya Thaha@DESKTOP-JKTS271>
2026-04-28 17:26:56 +0530

    Add project files.

---
+++ TaskManagement.Api/Controllers/TasksController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Api.Models;
+
+namespace TaskManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private static List<TaskItem> tasks = new List<TaskItem>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+        public IActionResult Create(TaskItem task)
+        {
+            task.Id = tasks.Count + 1;
+            tasks.Add(task);
+            return Ok(task);
+        }
+    }
+}
